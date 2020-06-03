693854e5efc1ad287f40f0a814528125a2ae83e8
Antti Alho <antti.s.alho@gmail.com>
2020-06-03 22:25:56 +0300

    part3

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 
@@ -31,8 +32,6 @@ let persons = [
     "id": 4
   }
 ]
-
-app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/info', (req, res) => {
   let string = `
