fda2cbe555729636ba5ba646c9f1afdbaed6994f
Antti Alho <antti.s.alho@gmail.com>
2020-06-03 22:29:17 +0300

    part3

---
+++ build/precache-manifest.d8956d99016ecb6d4fc9dfd684f8bc10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfdbbeaadb1caf401933e00cdcb0d29c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da5e2434c174cff09992",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "c9a736b30335e4a7f81c",
+    "url": "/static/js/2.42f56bda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42f56bda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5e2434c174cff09992",
+    "url": "/static/js/main.76fff677.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
