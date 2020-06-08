4bb46b9385ae0539dea7593e747d8ba52a4fd11a
Antti Alho <antti.s.alho@gmail.com>
2020-06-08 14:35:09 +0300

    heroku build

---
+++ build/precache-manifest.5b09369949b24cfcd62dfc3550543e47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53f4427da64b3ec7bb58212c54b4d69a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "332ff060ce3c2ee27f50",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "c9a736b30335e4a7f81c",
+    "url": "/static/js/2.42f56bda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42f56bda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "332ff060ce3c2ee27f50",
+    "url": "/static/js/main.4e5b1206.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
